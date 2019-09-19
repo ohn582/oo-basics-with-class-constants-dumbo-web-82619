77777bc79ec6820f2ae4c21157ddaacef1090343
Neal JIn Oh <no4259@gmail.com>
2019-09-19 04:40:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRANDS = ["Uggs", "Rainbow"]
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -15,4 +15,7 @@ class Shoe
   end
 end
   
+
+
+end
 
