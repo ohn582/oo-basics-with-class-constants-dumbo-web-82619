a56d5ef4e4b73c4bd6268fddf99718d2776e0a1b
Neal JIn Oh <no4259@gmail.com>
2019-09-19 04:40:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRANDS = []
+  BRANDS = ["Uggs", "Rainbow"]
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
@@ -15,7 +15,4 @@ class Shoe
   end
 end
   
-
-
-
 
